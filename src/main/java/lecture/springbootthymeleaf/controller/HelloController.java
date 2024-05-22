343582fe34ad25a2d54f7cdf26d1cdf3e8924c0a
imooxo <kimsi1127@gmail.com>
2024-05-22 12:29:35 +0900

    Thymeleaf_표현식-24.05.22

--- src/main/java/lecture/springbootthymeleaf/controller/HelloController.java
+++ src/main/java/lecture/springbootthymeleaf/controller/HelloController.java
@@ -11,26 +11,27 @@ public class HelloController {
 
     // localhost:8080/hi
     @GetMapping("/hi")
-    public String hello(Model model){
-        model.addAttribute("msg","hi~~");
-        model.addAttribute("msg2","<strong>안녕?</string>");
-        model.addAttribute("age",17);
-        model.addAttribute("userType","Admin"); // Admin, User, 그외..
+    public String hello(Model model) {
+        model.addAttribute("msg", "hi~~");
+        model.addAttribute("msg2", "<strong>안녕?</string>");
+        model.addAttribute("age", 17);
+        model.addAttribute("userType", "Admin"); // Admin, User, 그외..
 
         String[] fruits = {"apple", "banana", "cherry"};
-        model.addAttribute("fruits",fruits); //배열 넘기기
+        model.addAttribute("fruits", fruits); //배열 넘기기
 
-        model.addAttribute("url","https://google.co.kr");
+        model.addAttribute("url", "https://google.co.kr");
 
         Hello hello = new Hello();
-        model.addAttribute("hello",hello); // 객체 넘기기
+        model.addAttribute("hello", hello); // 객체 넘기기
 
         return "hi";
     }
 
-    class Hello{
+    class Hello {
         private String msg = "hi";
-        public String getMsg(){
+
+        public String getMsg() {
             return msg;
         }
     }//Hello class
