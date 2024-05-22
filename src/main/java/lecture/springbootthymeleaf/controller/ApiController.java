ce3c987e614ac6ce7d4852f7280dac276c53a3aa
imooxo <kimsi1127@gmail.com>
2024-05-22 16:02:17 +0900

    Springboot_API(Get,Post)-24.05.22

--- src/main/java/lecture/springbootthymeleaf/controller/ApiController.java
+++ src/main/java/lecture/springbootthymeleaf/controller/ApiController.java
@@ -2,15 +2,19 @@ package lecture.springbootthymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 // localhost:8080/api/~~~~~
 @Controller
 @RequestMapping("/api") //중간경로 지정
 public class ApiController {
+
+    // 컨트롤러에서 에러가 발생할 경우, @ExceptionHandler 가 catch 하여 해당 로직
+    @ExceptionHandler
+    public String errHandler(Exception e) {
+        System.out.println("error" + e.getMessage());
+        return "error";
+    }
 
     // GET localhost:8080/api
     @GetMapping("")
@@ -47,6 +51,35 @@ public class ApiController {
         model.addAttribute("name", name);
         model.addAttribute("age", age);
         return "res";
+    }
+
+    //     POST localhost:8080/api/res3 (name, age)
+    @PostMapping("/res3")
+    public String postRes(
+            @RequestParam String name,
+            @RequestParam int age,
+            Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "res";
+    }
+
+    @PostMapping("/res3/{param1}")
+    public String postRes2(
+            @RequestParam String param1,
+            @RequestParam String name,
+            @RequestParam int age,
+            Model model) {
+        System.out.println("param1: " + param1);
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "res";
+    }
+
+    @GetMapping("/res4")
+    @ResponseBody
+    public String getRes4() {
+        return "hello";
     }
 
 }//class
